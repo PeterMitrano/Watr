efc6da53b3cc968817ffc2b65ce55b31e272e2fe
Peter_Mitrano <mitranopeter@gmail.com>
2016-02-26 23:35:08 -0500

    removed settings example code and added report
    working on implementing HTTP post

--- android_app/Watr/app/src/main/java/watr/hack/watr/AsyncPoster.java
+++ android_app/Watr/app/src/main/java/watr/hack/watr/AsyncPoster.java
@@ -1,6 +1,14 @@
 package watr.hack.watr;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.MalformedInputException;
 
 /**
  * Created by peter on 2/26/16.
@@ -15,10 +23,27 @@ public class AsyncPoster extends AsyncTask<Void, Void, Boolean> {
 
     @Override
     protected Boolean doInBackground(Void... params) {
+        HttpURLConnection urlConnection = null;
+
+        try {
+            URL url = new URL("http://www.android.com/");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        }
+        catch(MalformedInputException e) {}
+        catch(IOException e) {}
+        finally {
+            urlConnection.disconnect();
+        }
+
         return null;
     }
 
-    public void onPostExecute(Boolean connected) {
+    private void readStream(InputStream in) {
+    }
 
+    public void onPostExecute(Boolean connected) {
+        Log.w("completed", "on post execute");
     }
 }
--- android_app/Watr/app/src/main/java/watr/hack/watr/Main.java
+++ android_app/Watr/app/src/main/java/watr/hack/watr/Main.java
@@ -3,12 +3,9 @@ package watr.hack.watr;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class Main extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,7 +16,7 @@ public class Main extends AppCompatActivity implements View.OnClickListener {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.createReportButton);
         fab.setOnClickListener(this);
     }
 
