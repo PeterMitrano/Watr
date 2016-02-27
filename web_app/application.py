4c51a4901e97cbf91bda471058f457b8e0bac87c
Andrew Walter <mist3rwalter@gmail.com>
2016-02-27 05:28:32 -0500

    Basic version of the website map

--- web_app/application.py
+++ web_app/application.py
@@ -1,56 +1,29 @@
 from flask import Flask, render_template, request
 import requests
+import sys
 
-api = 'https://api.github.com/'
-userURL = api + 'users/'
+api = 'http://localhost:3000/'
+reportEndpoint = 'reports/'
+try:
+    api_key_file = open('google_api_key', 'r')
+except FileNotFoundError:
+    print "API key file not found. Exiting..."
+    sys.exit(-1)
+api_key = api_key_file.read().rstrip("\n\r")
+api_key_file.close()
 
 # EB looks for an 'application' callable by default.
 application = Flask(__name__)
-
-weight_map = [
-        {'key': 'public_repos', 'weight': 10},
-        {'key': 'public_gists', 'weight': 1},
-        {'key': 'followers', 'weight': 20},
-        {'key': 'following', 'weight': 1}
-        ]
-
-messages = ["bad",
-        "weak",
-        "lame",
-        "passable",
-        "decent",
-        "worthy",
-        "good",
-        "great",
-        "amazing"]
-
-max_rank = 1000.0
 
 @application.route('/')
 def get_root():
     return render_template('index.html')
 
-@application.route('/rank')
-def get_predict():
-    user = request.args.get('user')
-    rank = None
-    if (user != None and user != ""):
-        rank = calculate_rank(user)
-    index = min(len(messages)-1, int((rank/max_rank) * (len(messages) - 1)))
-    message = messages[index]
-
-    return render_template('rank.html',
-            rank=rank,
-            message=message)
-
-def calculate_rank(user):
-    url = userURL + user
+@application.route('/map')
+def get_map():
+    url = api + reportEndpoint
     request = requests.get(url)
-    user = request.json()
-    score = 0
-    for element in weight_map:
-        score += element['weight'] * int(user[element['key']])
-    return score
+    return render_template('map.html', reports=request.json(), api_key=api_key)
 
 # run the application.
 if __name__ == '__main__':
