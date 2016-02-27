ab3a7622941ad39bc3c841aeb3c6716e5404e7c8
Peter_Mitrano <mitranopeter@gmail.com>
2016-02-26 23:55:09 -0500

    Added a fucking async poster to post HTTP data
    also upped API level cuz yolo motherfucker

--- android_app/Watr/app/src/main/java/watr/hack/watr/AsyncPoster.java
+++ android_app/Watr/app/src/main/java/watr/hack/watr/AsyncPoster.java
@@ -3,12 +3,15 @@ package watr.hack.watr;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.charset.MalformedInputException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by peter on 2/26/16.
@@ -23,24 +26,43 @@ public class AsyncPoster extends AsyncTask<Void, Void, Boolean> {
 
     @Override
     protected Boolean doInBackground(Void... params) {
-        HttpURLConnection urlConnection = null;
+
+        String urlParameters = "quality=" + report.quality
+                + "&town=" + report.town + "&state=" + report.state;
+        byte[] postData = new byte[0];
+        postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        int postDataLength = postData.length;
+
+        String requestURL = "http://httpbin.org/post";
 
         try {
-            URL url = new URL("http://www.android.com/");
-            urlConnection = (HttpURLConnection) url.openConnection();
-            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            readStream(in);
-        }
-        catch(MalformedInputException e) {}
-        catch(IOException e) {}
-        finally {
-            urlConnection.disconnect();
+            URL url = new URL(requestURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            conn.setDoOutput(true);
+            conn.setInstanceFollowRedirects(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("charset", "utf-8");
+            conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
+            conn.setUseCaches(false);
+
+            DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+            wr.write(postData);
+
+            Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            String str = "";
+            for (int c = in.read(); c != -1; c = in.read()) {
+                str += ((char)c);
+            }
+            Log.e("response", str);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
         return null;
-    }
-
-    private void readStream(InputStream in) {
     }
 
     public void onPostExecute(Boolean connected) {
